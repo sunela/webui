01dbbecdd6977e3fca80c7139565196a670a65a5
Werner Almesberger <werner@almesberger.net>
2024-08-19 12:44:56 -0300

    move sunela_* into a class (WIP)

--- rmt.js
+++ rmt.js
@@ -1,5 +1,15 @@
-// 
+// Useful references:
+//
+// WebUSB:
 // https://developer.mozilla.org/en-US/docs/Web/API/USB/requestDevice
+//
+// Typed Arrays (transfer buffers):
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypedArray/slice
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypedArray/toString
+//
+// Polling with delay:
+// https://stackoverflow.com/questions/46208031/polling-until-getting-specific-result
+
 
 const SUNELA_USB_VENDOR = 0x20b7;	/* Qi Hardware */
 const SUNELA_USB_PRODUCT = 0xae72;	/* Sunela (AnELok, device 2) */
@@ -11,92 +21,93 @@ const RDOP_LS = 2;
 const SUNELA_MAX_RESPONSE = 256;
 
 
-function sunela_send(device, data)
-{
-console.log("sending", data);
-	return device.controlTransferOut({
-		requestType:	"vendor",
-		recipient:	"device",
-		request:	SUNELA_RMT,
-		index:		0,
-		value:		0,
-	}, data);
-}
-
-
-function sunela_begin_request(device, op, arg)
-{
-	var data =  new Uint8Array([ op ]);
-
-	if (arg)
-		data += new TextEncoder("utf-8").encode(arg);
-	return sunela_send(device, data);
-}
-
-
-function sunela_end_request(device)
-{
-	return sunela_send(device, new Uint8Array(0));
-}
-
-
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypedArray/slice
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypedArray/toString
-
-
-// https://stackoverflow.com/questions/46208031/polling-until-getting-specific-result
-
-function wait(ms)
-{
-	return new Promise(resolve => { setTimeout(resolve, ms); });
-}
-
-
-async function sunela_receive(device)
-{
-	var res;
-
-	while (1) {
-		res = await device.controlTransferIn({
+class Sunela {
+	send(data)
+	{
+		console.log("sending", data);
+		return this.device.controlTransferOut({
 			requestType:	"vendor",
 			recipient:	"device",
 			request:	SUNELA_RMT,
 			index:		0,
 			value:		0,
-		}, SUNELA_MAX_RESPONSE);
-		if (res.status != "stall")
-			break;
-		await wait(100);
+		}, data);
 	}
-	if (res.status == "ok") {
-		var decoder = new TextDecoder();
-		return decoder.decode(res.data);
+
+
+	begin_request(op, arg)
+	{
+		var data =  new Uint8Array([ op ]);
+
+		if (arg)
+			data += new TextEncoder("utf-8").encode(arg);
+		return this.send(data);
 	}
-	console.log("controlTransferIn status:", res.status);
-	return null;
-}
 
 
-async function request_usb()
-{
-	let device;
+	end_request()
+	{
+		return this.send(new Uint8Array(0));
+	}
 
-	device = await navigator.usb
-	    .requestDevice({ filters: [{
-		vendorId: SUNELA_USB_VENDOR,
-		productId: SUNELA_USB_PRODUCT }]});
 
-	await device.open();
-	await device.selectConfiguration(1);
-//	await device.claimInterface(0);
+	wait(ms)
+	{
+		return new Promise(resolve => { setTimeout(resolve, ms); });
+	}
 
-	sunela_begin_request(device, RDOP_LS, null);
-	sunela_end_request(device);
 
-	while (1) {
-		got = await sunela_receive(device);
-		if (got == "")
-			break;
-		console.log(got);
+	async receive()
+	{
+		var res;
+
+		while (1) {
+			res = await this.device.controlTransferIn({
+				requestType:	"vendor",
+				recipient:	"device",
+				request:	SUNELA_RMT,
+				index:		0,
+				value:		0,
+			}, SUNELA_MAX_RESPONSE);
+			if (res.status != "stall")
+				break;
+			await this.wait(100);
+		}
+		if (res.status == "ok") {
+			var decoder = new TextDecoder();
+			return decoder.decode(res.data);
+		}
+		console.log("controlTransferIn status:", res.status);
+		return null;
+	}
+
+
+	async request_usb()
+	{
+		this.device = await navigator.usb
+		    .requestDevice({ filters: [{
+			vendorId: SUNELA_USB_VENDOR,
+			productId: SUNELA_USB_PRODUCT }]});
+
+		await this.device.open();
+		await this.device.selectConfiguration(1);
+//		await this.device.claimInterface(0);
+
+		await this.begin_request(RDOP_LS, null);
+		await this.end_request();
+
+		while (1) {
+			var got = await this.receive();
+
+			if (got == "")
+				break;
+			console.log(got);
+		}
+	}
+
+
+	constructor()
+	{
+		this.device = null;
 	}
 }
