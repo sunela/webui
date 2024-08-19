2b8c85234c0a51f4264f66fa120c95aebc430329
Werner Almesberger <werner@almesberger.net>
2024-08-19 14:16:59 -0300

    add MIT license; add copyright headers

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+/*
+ * main.js - Sunela Web UI
+ *
+ * This work is licensed under the terms of the MIT License.
+ * A copy of the license can be found in the file LICENSE.MIT
+ */
+
 /* --- Useful functions ---------------------------------------------------- */
 
 
--- rmt.js
+++ rmt.js
@@ -1,3 +1,10 @@
+/*
+ * rmt.css - Sunela remote consrol protocol (experimental)
+ *
+ * This work is licensed under the terms of the MIT License.
+ * A copy of the license can be found in the file LICENSE.MIT
+ */
+
 // Useful references:
 //
 // WebUSB:
