a880b28803231563e83635dfcf95f05f49cac299
Werner Almesberger <werner@almesberger.net>
2024-09-04 23:37:03 -0300

    rmt.js: implement reveal, get_time, and set_time operations

--- rmt.js
+++ rmt.js
@@ -21,15 +21,16 @@
 const SUNELA_USB_VENDOR = 0x20b7;	/* Qi Hardware */
 const SUNELA_USB_PRODUCT = 0xae72;	/* Sunela (AnELok, device 2) */
 
-const SUNELA_RMT = 4;
+const SUNELA_RMT	= 4;
 
-const RDOP_LS = 2;
-const RDOP_SHOW = 4;
+const RDOP_LS		= 2;
+const RDOP_SHOW		= 4;
+const RDOP_REVEAL	= 5;
+const RDOP_GET_TIME	= 6;
+const RDOP_SET_TIME	= 7;
 
 const SUNELA_MAX_RESPONSE = 256;
 
-
-var ggg = null;
 
 class Sunela {
 
@@ -147,6 +148,59 @@ class Sunela {
 		await this.end_request();
 		res = await this.response();
 		return res.map((x) => new Uint8Array(x));
+	}
+
+
+	async reveal(name, field)
+	{
+		var data;
+
+		data = new Uint8Array(name.length + 2);
+		data.set(new Uint8Array([ RDOP_REVEAL ]));
+		data.set(new TextEncoder("utf-8").encode(name), 1);
+		data.set(new Uint8Array([ field ]), name.length + 1);
+
+		await this.send(data);
+		await this.end_request();
+		await this.response();
+	}
+
+
+	async get_time()
+	{
+		var res;
+
+		await this.begin_request(RDOP_GET_TIME, null);
+		await this.end_request();
+		res = await this.response();
+		/*
+		 * The time is stored in little-endian byte order in the first
+		 * (and only) response record, preceded by a 01 byte (to make
+		 * sure the first byte won't be zero, which would be an error
+		 * response.)
+		 */
+		res = new Uint8Array(res[0]);
+		res.reverse();
+		return res.slice(0, 8).reduce((a, x) => a << 8 | x, 0);
+	}
+
+
+	async set_time(t)
+	{
+		var data;
+
+		if (t === null)
+			t = Math.floor(Date.now() / 1000);
+		data = new Uint8Array(9);
+		data[0] = RDOP_SET_TIME;
+		for (var i = 0; i != 8; i++) {
+			data[i + 1] = t & 0xff;
+			t >>= 8;
+		}
+
+		await this.send(data);
+		await this.end_request();
+		await this.response();
 	}
 
 
